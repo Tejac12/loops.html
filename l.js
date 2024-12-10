f6123310ea21ac3831d29482e3b5c2e22e4a8d5a
Tejac12 <chintalatejavardhan692@gmail.com>
2024-12-10 16:25:19 +0530

    Add files via upload

---
+++ l.js
@@ -0,0 +1,63 @@
+//for loop
+//post increment
+for (var a = 1; a < 3; a++) {
+    console.log(a)
+}
+//post decrement
+for (var a = 24; a >= 15; a--) {
+    console.log(a)
+}
+
+
+//while loop
+//post increment
+var a = 1;
+while (a <= 7) {
+    console.log(a)
+    a++
+}
+//ex2
+var a = 1;
+while (a < 3) {
+    console.log(a)
+    a++
+}
+
+//post decrement
+var a = 10;
+while (a >= 6) {
+    console.log(a)
+    a--
+}
+
+
+
+//do while exzmples
+
+//post increment
+
+var a = 1;
+do {
+    a++
+    console.log(a)
+} while (a < 3)
+
+var a = 7;
+do {
+    a++
+    console.log(a)
+} while (a < 15)
+
+//post decrement
+var a = 24;
+do {
+    a--
+    console.log(a)
+} while (a >= 12)
+
+
+var a = 10;
+do {
+    console.log(a)
+    a--
+} while (a >= 7)
